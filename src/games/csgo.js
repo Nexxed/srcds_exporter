b08001f21b24651d40b61bb07bc870a9561fc62e
nex <headstruck@protonmail.ch>
2023-03-31 14:04:30 +0100

    feat: player ping/loss/rate metrics, improved parsing

--- src/games/csgo.js
+++ src/games/csgo.js
@@ -1,69 +1,99 @@
-const { csgoRegistry } = require('../utils/metrics.js').registries;
+const { csgoRegistry } = require("../utils/metrics.js").registries
 
-const { metrics } = require('../utils/metrics.js');
+const { metrics } = require("../utils/metrics.js")
+
+const playerRowRegex =
+	/^#  (?<userid>[0-9]+) (?:.*?) "(?<name>.*?)" (?<uniqueid>STEAM_[10]:[10]:[0-9]+) (?<connected>[0-9]+:[0-9]+) (?<ping>[0-9]+) (?<loss>[0-9]+) (?<state>.*?) (?<rate>[0-9]+) (?<address>.*?):(?<port>[0-9]+)$/
 
 const formatRconResult = function (result) {
-    let { stats, status } = result;
+	let { stats, status } = result
 
-    stats = stats.split(/\r?\n/);
-    stats.pop();
-    stats.shift();
-    stats = stats[0].trim().split(/\s+/);
+	stats = stats.split(/\r?\n/)
+	stats.pop()
+	stats.shift()
+	stats = stats[0].trim().split(/\s+/)
 
-    const infosArray = status.split(/\r?\n/);
+	const infosArray = status.split(/\r?\n/)
 
-    status = {
-        hostname: infosArray[0].split(': ').slice(1).join(': '),
-        version: infosArray[1].split(': ')[1].split('/')[0],
-        map: infosArray[5].split(': ')[1],
-    }
+	status = {
+		hostname: infosArray[0].split(": ").slice(1).join(": "),
+		version: infosArray[1].split(": ")[1].split("/")[0],
+		listenAddress: infosArray[2].split(": ")[1].split("  ")[0],
+		os: infosArray[3].split(":  ")[1],
+		type: infosArray[4].split(":  ")[1],
+		map: infosArray[5].split(": ")[1],
 
-    return {
-        stats,
-        status
-    };
+		players: infosArray
+			.slice(
+				infosArray.findIndex((row) => row.startsWith("# userid")) + 1,
+				infosArray.findIndex((row) => row === "#end")
+			)
+			.map((row) => {
+				const matches = row.match(playerRowRegex)
+				if (!matches || !matches.groups) return false
+
+				const obj = {}
+				Object.assign(obj, matches.groups)
+				return obj
+			})
+			.filter((r) => r)
+	}
+
+	return {
+		stats,
+		status
+	}
 }
 
 const setMetrics = function (result, reqInfos) {
-    const { stats, status } = formatRconResult(result);
+	const { stats, status } = formatRconResult(result)
 
-    const defaultLabels = {
-        server: `${reqInfos.ip}:${reqInfos.port}`,
-        game: reqInfos.game,
-        version: status.version,
-        hostname: status.hostname,
-        map: status.map,
-    };
-    csgoRegistry.setDefaultLabels(defaultLabels);
+	const defaultLabels = {
+		server: `${reqInfos.ip}:${reqInfos.port}`,
+		game: reqInfos.game,
+		version: status.version,
+		hostname: status.hostname,
+		map: status.map,
+		os: status.os,
+		type: status.type
+	}
+	csgoRegistry.setDefaultLabels(defaultLabels)
 
-    metrics.status.set((Number(1)));
-    metrics.cpu.set((Number(stats[0])));
-    metrics.netin.set((Number(stats[1])));
-    metrics.netout.set((Number(stats[2])));
-    metrics.uptime.set((Number(stats[3])));
-    metrics.maps.set((Number(stats[4])));
-    metrics.fps.set((Number(stats[5])));
-    metrics.players.set((Number(stats[6])));
-    metrics.svms.set((Number(stats[7])));
-    metrics.varms.set((Number(stats[8])));
-    metrics.tick.set((Number(stats[9])));
+	metrics.status.set(Number(1))
+	metrics.cpu.set(Number(stats[0]))
+	metrics.netin.set(Number(stats[1]))
+	metrics.netout.set(Number(stats[2]))
+	metrics.uptime.set(Number(stats[3]))
+	metrics.maps.set(Number(stats[4]))
+	metrics.fps.set(Number(stats[5]))
+	metrics.players.set(Number(stats[6]))
+	metrics.svms.set(Number(stats[7]))
+	metrics.varms.set(Number(stats[8]))
+	metrics.tick.set(Number(stats[9]))
 
-    return csgoRegistry.metrics();
+	for (const player of status.players) {
+		const identifiers = { id: player.userid, name: player.name, uniqueid: player.uniqueid }
+		metrics.player_ping.set(identifiers, Number(player.ping))
+		metrics.player_loss.set(identifiers, Number(player.loss))
+		metrics.player_rate.set(identifiers, Number(player.rate))
+	}
+
+	return csgoRegistry.metrics()
 }
 
 setNoMetrics = function (reqInfos) {
-    const defaultLabels = {
-        server: `${reqInfos.ip}:${reqInfos.port}`,
-        game: reqInfos.game,
-    };
-    csgoRegistry.setDefaultLabels(defaultLabels);
+	const defaultLabels = {
+		server: `${reqInfos.ip}:${reqInfos.port}`,
+		game: reqInfos.game
+	}
 
-    metrics.status.set((Number(0)));
+	csgoRegistry.setDefaultLabels(defaultLabels)
+	metrics.status.set(Number(0))
 
-    return csgoRegistry.metrics();
+	return csgoRegistry.metrics()
 }
 
 module.exports = {
-    setMetrics,
-    setNoMetrics
+	setMetrics,
+	setNoMetrics
 }
